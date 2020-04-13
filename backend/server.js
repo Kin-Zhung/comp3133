08ef86ccc355b2ddbe54b6b7d7ce0b8009529c49
Jeremy <li-jeremy@outlook.com>
2020-04-13 13:24:31 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio= require('socket.io');
 const http = require('http');
+const path = require('path');
 const mongoose = require('mongoose');
 const Message = require('./model/message')
 const EventLog = require('./model/eventLog')
@@ -15,8 +16,16 @@ const PORT = process.env.PORT || 5000;
 const router = require('./router')
 
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+if(process.env.NODE_ENV ==='production'){
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
+
+//const server = http.createServer(app);
+//const io = socketio(server);
 
 io.on('connection',(socket)=>{
     console.log('user connected');
